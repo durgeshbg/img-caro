b4842b19544690ab2091e00ac307fd9cf75ca6d9
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-12-24 18:25:17 +0530

    fix: change length to childElementCount

--- index.js
+++ index.js
@@ -2,19 +2,18 @@ display = function (frameElement, carousalElement, imgID) {
   const prevImg = frameElement.removeChild(frameElement.firstElementChild);
   carousalElement.appendChild(prevImg);
   frameElement.appendChild(document.getElementById(imgID));
-  activeImg();
 };
 
 exports.moveLeft = function (frameElement, carousalElement) {
-  const len = carousalElement.length;
+  const len = carousalElement.childElementCount;
   const prevImg = frameElement.firstElementChild;
-  const nextImgID = (Number(prevImg.id) + 7) % len;
+  const nextImgID = (Number(prevImg.id) + len - 1) % len;
 
   display(frameElement, carousalElement, nextImgID);
 };
 
 exports.moveRight = function (frameElement, carousalElement) {
-  const len = carousalElement.length;
+  const len = carousalElement.childElementCount;
   const prevImg = frameElement.firstElementChild;
   const nextImgID = (Number(prevImg.id) + 1) % len;
 
